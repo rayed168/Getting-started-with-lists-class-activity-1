2596c8834dca4a2114299c1c9972202a186bab59
rayed168 <muhammad.rayed2013@gmail.com>
2024-09-16 16:01:16 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,9 @@
+fruits=["apple","banana","oramge","mango","watermelon"]
+print(fruits)
+print(fruits[3])
+print(fruits[1:4])
+numbers=[1,2,3,4,5,6,7,8,9,10]
+print(numbers)
+print(numbers[::-1])
+print(numbers[-6::-2])
+print(numbers*3)
